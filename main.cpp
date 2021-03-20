120dfc70fded1bf3e7d5d1ce2d428d1094fc73fd
nuhaaali <71837816+nuhaaali@users.noreply.github.com>
2021-03-20 19:13:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 /*
-Author: Mariyam Nuha Ali
-description: stay safe and wear mask
+Author: Mariyam Nuha ALi
+Description: different heart beats
+
 */
 
 #include "MicroBit.h"
@@ -13,27 +14,41 @@ int main()
     uBit.init();
 
     // Insert your code here!
-    uBit.display.scroll("HELLO WORLD! ");
+    uBit.display.scroll("HEART BEAT");
+
     
-    //While - loop for button implementation
-    while(1)
+    // While - Loop for button implementation
+    while (1)
     {
-        // if statement is passed for button specific LED command
-        // button A for specific LED commands
-        if (uBit.buttonA.isPressed())
-        {
-            uBit.display.scroll(" STAY SAFE ");
-        }
-        
-        // button B for specific LED commands
-        if (uBit.buttonB.isPressed())
-        {
-            uBit.display.scroll(" WEAR MASK :) ");
-        }
+        // if statement is passed for button specifc LED command
+        // Button A for specific LED command
+    if (uBit.buttonA.isPressed())
+    {
+        MicroBitImage empty_heart("0,255,0,255,0\n 255,0,255,0,255\n 255,0,0,0,255\n 0,255,0,255,0\n 0,0,255,0,0\n");
+
+        uBit.display.print(empty_heart);
     }
+    
+    // Button B for specific LED command
+    if (uBit.buttonB.isPressed())
+    {
+          MicroBitImage full_heart("0,255,0,255,0\n 255,255,255,255,255\n 255,255,255,255,255\n 0,255,255,255,0\n 0,0,255,0,0\n");
+
+          uBit.display.print(full_heart);
+    }
+    
+     if (uBit.buttonAB.isPressed())
+    {
+      MicroBitImage small_heart("0,0,0,0,0\n 0,255,0,255,0\n 0,255,255,255,0\n 0,0,255,0,0\n 0,0,0,0,0\n");
+
+      uBit.display.print(small_heart); 
+    }
+    }
+
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
     // sit in the idle task forever, in a power efficient sleep.
     release_fiber();
 }
+
